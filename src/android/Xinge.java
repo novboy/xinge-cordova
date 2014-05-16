3f8d9b06bd1a6ffcd03ed5dc184a60b1aa18695c
tiger.wang <ahoo@hroze.org>
2014-05-16 17:12:47 +0800

    add missing method

--- src/android/Xinge.java
+++ src/android/Xinge.java
@@ -2,12 +2,14 @@ package org.apache.cordova.xinge;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.util.Log;
 
 import com.tencent.android.tpush.XGIOperateCallback;
+import com.tencent.android.tpush.XGLocalMessage;
 import com.tencent.android.tpush.XGPushConfig;
 import com.tencent.android.tpush.XGPushManager;
 
@@ -21,7 +23,7 @@ public class Xinge extends CordovaPlugin {
      * @return                  A PluginResult object with a status and message.
      */
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    	Log.d("TPush", "execute action"+action);
+    	Log.d("TPush", "execute action:"+action+" with args:"+args);
     	 if ("register".equals(action)) {
              //this.beep(args.getLong(0));
     		 String account = args.getString(0);
@@ -33,18 +35,72 @@ public class Xinge extends CordovaPlugin {
         	 String accessKey = args.getString(1);
         	 boolean debug = args.getBoolean(2);
         	 return	configure(accessId,accessKey,debug,callbackContext);
+         }else if("onMessage".equals(action)) {
+        	 return	onMessage(callbackContext);
+         }else if("setTag".equals(action)) {
+        	 String tag = args.getString(0);
+        	 return	setTag(tag,callbackContext);
+         }else if("deleteTag".equals(action)) {
+        	 String tag = args.getString(0);
+        	 return	deleteTag(tag,callbackContext);
+         }else if("notify".equals(action)) {
+        	 String title = args.getString(0);
+        	 String content = args.getString(1);
+        	 return	notify(title,content,callbackContext);
          }
          return false;  // Returning false results in a "MethodNotFound" error.
+    }
+    public boolean setTag(String tag,final CallbackContext callbackContext) {
+    	XGPushManager.setTag(this.cordova.getActivity(), tag);
+    	callbackContext.success();
+    	return true;
+    }
+    public boolean deleteTag(String tag,final CallbackContext callbackContext) {
+    	XGPushManager.deleteTag(this.cordova.getActivity(), tag);
+    	callbackContext.success();
+    	return true;
+    }
+    /**
+     * send local notify
+     * @param title
+     * @param content
+     * @param callbackContext
+     * @return
+     */
+    public boolean notify(String title,String content,final CallbackContext callbackContext) {
+    	XGLocalMessage localMessage = new XGLocalMessage();
+    	localMessage.setContent(content);
+    	localMessage.setTitle(title);
+    	localMessage.setType(1);
+    	//localMessage.setBuilderId(XGPushConfig.);
+    	Long msgId = XGPushManager.addLocalNotification(this.cordova.getActivity(),localMessage);
+    	callbackContext.success(""+msgId);
+    	return true;
     }
     public boolean configure(Long accessId,String accessKey,boolean debug,final CallbackContext callbackContext) {
     	XGPushConfig.setAccessId(this.cordova.getActivity(), accessId);
     	XGPushConfig.setAccessKey(this.cordova.getActivity(), accessKey);
     	XGPushConfig.enableDebug(this.cordova.getActivity(), debug);
+    	XGPushManager.onActivityStarted(this.cordova.getActivity());
     	callbackContext.success();
     	return true;
     }
+    
+    public boolean onMessage(final CallbackContext callbackContext) {
+    	XGPushCordovaReceiver.msgCallbackContext = callbackContext;
+    	PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+		pluginResult.setKeepCallback(true);
+		callbackContext.sendPluginResult(pluginResult);
+    	return true;
+    }
+    public boolean onClick(final CallbackContext callbackContext) {
+    	XGPushCordovaReceiver.openCallbackContext = callbackContext;
+    	PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+		pluginResult.setKeepCallback(true);
+		callbackContext.sendPluginResult(pluginResult);
+    	return true;
+    }
     public boolean register(String account,final CallbackContext callbackContext) {
-    	XGPushManager.unregisterPush(this.cordova.getActivity());
     	XGPushManager.registerPush(this.cordova.getActivity(), account,
     			new XGIOperateCallback() {
     				@Override
--- www/Xinge.js
+++ www/Xinge.js
@@ -1,7 +1,7 @@
 var exec = require("cordova/exec");
 var xingeExports = {};
 
-xingeExports.register = function (successCallback, errorCallback, account) {
+xingeExports.register = function (account,successCallback, errorCallback) {
     this.successCallback = successCallback;
     
     if (errorCallback == null) {
@@ -31,7 +31,7 @@ xingeExports.unregister = function (successCallback, errorCallback) {
 
     cordova.exec(successCallback, errorCallback, "Xinge", "unregister", []);
 };
-xingeExports.configure = function (successCallback, errorCallback,accessId,accessKey,debug) {
+xingeExports.configure = function (accessId,accessKey,debug,successCallback, errorCallback) {
     if (errorCallback == null) {
         errorCallback = function () {
         }
@@ -43,6 +43,68 @@ xingeExports.configure = function (successCallback, errorCallback,accessId,acces
     }
 	
     cordova.exec(successCallback, errorCallback, "Xinge", "configure", [accessId,accessKey,debug]);
+};
+
+xingeExports.onMessage = function (successCallback, errorCallback) {
+    if (errorCallback == null) {
+        errorCallback = function () {
+        }
+    }
+
+    if (typeof successCallback != "function") {
+        console.log("Xinge.onMessage failure: success callback parameter must be a function");
+        return;
+    }
+	
+    cordova.exec(successCallback, errorCallback, "Xinge", "onMessage", []);
+};
+xingeExports.notify = function (title,content,successCallback, errorCallback) {
+    if (errorCallback == null) {
+        errorCallback = function () {
+        }
+    }
+	if (successCallback == null) {
+        successCallback = function (data) {
+			console.log("notify "+title+":"+data);
+        }
+    }
+    cordova.exec(successCallback, errorCallback, "Xinge", "notify", [title,content]);
+};
+xingeExports.deleteTag = function (tag,successCallback, errorCallback) {
+    if (errorCallback == null) {
+        errorCallback = function () {
+        }
+    }
+	if (successCallback == null) {
+        successCallback = function (data) {
+			console.log("delete tag "+tag+":"+data);
+        }
+    }
+    cordova.exec(successCallback, errorCallback, "Xinge", "deleteTag", [tag]);
+};
+xingeExports.setTag = function (tag,successCallback, errorCallback) {
+    if (errorCallback == null) {
+        errorCallback = function () {
+        }
+    }
+	if (successCallback == null) {
+        successCallback = function (data) {
+			console.log("add tag "+tag+":"+data);
+        }
+    }
+    cordova.exec(successCallback, errorCallback, "Xinge", "setTag", [tag]);
+};
+
+xingeExports.onClick = function (successCallback, errorCallback) {
+    if (errorCallback == null) {
+        errorCallback = function () {
+        }
+    }
+    if (typeof successCallback != "function") {
+        console.log("Xinge.unregister failure: success callback parameter must be a function");
+        return;
+    }
+    cordova.exec(successCallback, errorCallback, "Xinge", "onClick", []);
 };
 
 module.exports = xingeExports;
